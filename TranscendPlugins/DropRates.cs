8f04fc214c3b2141eab1fa1c1e8e9c6c1f3a6f34
Doug <dougbenham@cox.net>
2015-07-21 19:27:50 -0700

    1.2.0.8 updates

---
+++ TranscendPlugins/DropRates.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using PluginLoader;
+using Terraria;
+
+namespace BlahPlugins
+{
+    public class DropRates : MarshalByRefObject, IPluginNPCLoot
+    {
+        private byte factor = 1;
+        private Keys inc, dec;
+        private bool recursionFlag = false;
+
+        public DropRates()
+        {
+            if (!Keys.TryParse(IniAPI.ReadIni("DropRates", "IncKey", "P", writeIt: true), out inc)) inc = Keys.P;
+            if (!Keys.TryParse(IniAPI.ReadIni("DropRates", "DecKey", "O", writeIt: true), out dec)) dec = Keys.O;
+            factor = byte.Parse(IniAPI.ReadIni("DropRates", "Factor", "1", writeIt: true));
+
+            Color yellow = Color.Yellow;
+            Loader.RegisterHotkey(() =>
+            {
+                if (factor < 20)
+                    factor++;
+                IniAPI.WriteIni("DropRates", "Factor", factor.ToString());
+                Main.NewText("Drop Rates multiplied by " + factor, yellow.R, yellow.G, yellow.B, false);
+            }, inc);
+
+            Loader.RegisterHotkey(() =>
+            {
+                if (factor > 0)
+                    factor--;
+                IniAPI.WriteIni("DropRates", "Factor", factor.ToString());
+                Main.NewText("Drop Rates multiplied by " + factor, yellow.R, yellow.G, yellow.B, false);
+            }, dec);
+        }
+
+        public bool OnNPCLoot(NPC npc)
+        {
+            if (recursionFlag) return false; // flag is set, avoid recursion
+
+            recursionFlag = true;
+            for (int i = 0; i < factor; i++)
+                npc.NPCLoot();
+            recursionFlag = false;
+
+            return true;
+        }
+    }
+}
