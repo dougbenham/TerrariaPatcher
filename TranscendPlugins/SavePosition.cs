4169ee6366f8307bcdbaa06fd329a5b815b460cc
Doug Benham <douglasabenham@gmail.com>
2026-08-20 16:30:04 -0700

    Fixed decimal globalization

--- TranscendPlugins/SavePosition.cs
+++ TranscendPlugins/SavePosition.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -10,6 +11,8 @@ namespace TranscendPlugins
     [PluginDescription("Remembers where you were when you left a world and puts you back there instead of at your spawn point.")]
     public class SavePosition : PluginBase, IPluginPlayerLoad, IPluginPlayerSave, IPluginPlayerSpawn
     {
+        private const string Section = "SavePosition";
+
         private bool justLoadedIn = false;
 
         public void OnPlayerSave(PlayerFileData playerFileData, Player player, BinaryWriter binaryWriter)
@@ -19,33 +22,61 @@ namespace TranscendPlugins
             if (Main.worldID == 0) return;
             if (player.position.X == 0f && player.position.Y == 0f) return;
 
-            IniAPI.WriteIni("SavePosition", Main.worldID + "," + player.name, player.position.ToString());
+            var value = player.position.X.ToString("R", CultureInfo.InvariantCulture) + " " +
+                        player.position.Y.ToString("R", CultureInfo.InvariantCulture);
+
+            IniAPI.WriteIni(Section, GetKey(player.name), value);
         }
 
         public void OnPlayerLoad(PlayerFileData playerFileData, Player player, BinaryReader binaryReader)
         {
             justLoadedIn = true;
         }
-		
+
         public void OnPlayerSpawn(Player player)
         {
             if (player.whoAmI != Main.myPlayer || !justLoadedIn) return;
 
-            var vector = IniAPI.ReadIni("SavePosition", Main.worldID + "," + Main.player[Main.myPlayer].name, null);
-            if (!string.IsNullOrEmpty(vector))
-            {
-                int startIndX = vector.IndexOf("X:") + 2;
-                int startIndY = vector.IndexOf("Y:") + 2;
-                var x = float.Parse(vector.Substring(startIndX, vector.IndexOf(" Y") - startIndX));
-                var y = float.Parse(vector.Substring(startIndY, vector.IndexOf("}") - startIndY));
-                var position = new Vector2(x, y);
-                
-                Player.Teleport(position, 1, 0);
-                Player.velocity = Vector2.Zero;
-                NetMessage.SendData(65, -1, -1, null, 0, Player.whoAmI, position.X, position.Y, 1, 0, 0);
-            }
-
             justLoadedIn = false;
+
+            var value = IniAPI.ReadIni(Section, GetKey(Main.player[Main.myPlayer].name), null);
+            Vector2 position;
+            if (!TryParsePosition(value, out position)) return;
+            if (!IsInsideWorld(position)) return;
+
+            Player.Teleport(position, 1, 0);
+            Player.velocity = Vector2.Zero;
+            NetMessage.SendData(65, -1, -1, null, 0, Player.whoAmI, position.X, position.Y, 1, 0, 0);
+        }
+
+        private static string GetKey(string playerName)
+        {
+            return Main.worldID.ToString(CultureInfo.InvariantCulture) + "," + playerName;
+        }
+
+        private static bool IsInsideWorld(Vector2 position)
+        {
+            return position.X >= 0f && position.X <= Main.maxTilesX * 16f
+                && position.Y >= 0f && position.Y <= Main.maxTilesY * 16f;
+        }
+
+        private static bool TryParsePosition(string value, out Vector2 position)
+        {
+            position = Vector2.Zero;
+            if (string.IsNullOrEmpty(value)) return false;
+
+            // Accepts "x y" as well as the Vector2.ToString() form "{X:x Y:y}" written by earlier versions.
+            var text = value.Replace("{", " ").Replace("}", " ").Replace("X:", " ").Replace("Y:", " ").Replace(",", " ");
+            var parts = text.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length != 2) return false;
+
+            return TryParseCoordinate(parts[0], out position.X)
+                && TryParseCoordinate(parts[1], out position.Y);
+        }
+
+        private static bool TryParseCoordinate(string text, out float value)
+        {
+            return float.TryParse(text, NumberStyles.Float, CultureInfo.InvariantCulture, out value);
         }
     }
 }
